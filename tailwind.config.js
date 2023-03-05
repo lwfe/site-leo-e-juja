c3abd692926798749b6dbb63f171149e9df04310
lwfe <leonardolwfe@gmail.com>
2023-03-05 00:04:41 -0300

    added color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
  
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
+    colors: {
+      'dark-emo': '#191B18',
+      'rocha': '#404241',
+      'pano-de-chao': '#969090',
+      'cimento': '#CCC7C1',
+      'arroz':'#E2D8D7',
+      'coreano': '#E5B8A5',
+      'creme-de-morango': '#E36569',
+      'amora-trans': '#B92E41',
+      'amora-cis': '#972131',
+      'amorinha': '#6D161F',
+    }
   },
   plugins: [],
 }
